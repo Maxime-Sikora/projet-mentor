1ca2fd364e233972f9c545f4cd48d77372f0a9a2
Maxime-Sikora <maxime.sikora@arkios.fr>
2024-10-11 15:05:47 +0200

    add webhook stripe

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -5,9 +5,14 @@ import Stripe from 'stripe';
 @Injectable()
 export class StripeService {
   private readonly stripe: Stripe;
+  private readonly webhookKey: string;
   constructor(private configService: ConfigService) {
     const stripeApiKey = this.configService.get('STRIPE_SECRET_KEY');
+    const stripeWebHookKey = this.configService.get(
+      'STRIPE_SECRET_WEBHOOK_KEY',
+    );
     this.stripe = new Stripe(stripeApiKey);
+    this.webhookKey = stripeWebHookKey;
   }
   public async createPaymentIntent(params: Stripe.PaymentIntentCreateParams) {
     return this.stripe.paymentIntents.create(params);
@@ -18,5 +23,25 @@ export class StripeService {
     params?: Stripe.PaymentIntentCaptureParams,
   ) {
     return this.stripe.paymentIntents.capture(id, params);
+  }
+
+  public async handleIncomingEvents(signature: string, rawBody: Buffer) {
+    const event = await this.stripe.webhooks.constructEvent(
+      rawBody,
+      signature,
+      this.webhookKey,
+    );
+    switch (event.type) {
+      case 'payment_intent.created':
+        console.log('payment intent created');
+        break;
+      case 'payment_intent.succeeded':
+        console.log('payement intent succeeded');
+        break;
+      case 'payment_intent.payment_failed':
+        console.log('payement intent failed');
+        break;
+    }
+    return true;
   }
 }
